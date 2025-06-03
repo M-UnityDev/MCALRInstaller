46fb206c0d5e72d7709fd29bf6406a764c512d79
M-UnityDev <mihailsheshukov10@gmail.com>
2025-06-03 22:46:16 +0500

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 WorkDir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-sudo pacman -S hyprland waybar fastfetch fish wofi uwsm kitty hyprpaper hyprlock pcmanfm-gtk3 hyprlock nwg-look qalculate-gtk gvfs superfile ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono pipewire wireplumber xarchiver wl-clipboard cliphist grim hyprpolkitagent
+sudo pacman -S hyprland waybar fastfetch fish wofi uwsm kitty hyprpaper hyprlock pcmanfm-gtk3 nwg-look qalculate-gtk gvfs superfile ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono pipewire wireplumber xarchiver wl-clipboard cliphist grim hyprpolkitagent slurp
+[ ! -d "~/MCALRUpdater/" ] && mkdir ~/MCALRUpdater/
+cp -f $WorkDir/UpdaterFiles/* ~/MCALRUpdater/
+/bin/bash ~/MCALRUpdater/Updater.sh
 if ! command -v yay >/dev/null; then
 	git clone -q https://aur.archlinux.org/yay.git $WorkDir/yay/
 	cd $WorkDir/yay/
@@ -8,7 +11,4 @@ if ! command -v yay >/dev/null; then
 	cd $WorkDir
 	rm -rf $WorkDir/yay/
 fi
-yay -S adwaita-colors-icon-theme
-[ ! -d "~/MCALRUpdater/" ] && mkdir ~/MCALRUpdater/
-cp -f $WorkDir/UpdaterFiles/* ~/MCALRUpdater/
-/bin/bash ~/MCALRUpdater/Updater.sh
+yay -S adwaita-colors-icon-theme zen-browser-bin github-desktop-bin
