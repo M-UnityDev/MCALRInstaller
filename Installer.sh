adb64c39a11df6754d027cb480598161bb233ae5
M-UnityDev <mihailsheshukov10@gmail.com>
2025-06-04 03:33:55 +0500

    MORE PACKAGES

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WorkDir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-sudo pacman -S hyprland waybar fastfetch fish wofi uwsm kitty hyprpaper hyprlock pcmanfm-gtk3 nwg-look qalculate-gtk gvfs superfile ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono pipewire wireplumber xarchiver wl-clipboard cliphist grim hyprpolkitagent slurp
+sudo pacman -S hyprland waybar fastfetch fish wofi uwsm kitty hyprpaper hyprlock pcmanfm-gtk3 nwg-look qalculate-gtk gvfs superfile ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono pipewire wireplumber xarchiver wl-clipboard cliphist grim hyprpolkitagent noto-fonts-extra plymouth mako noto-fonts-emoji imagemagick bashtop ttf-awesome-font slurp
 [ ! -d "~/MCALRUpdater/" ] && mkdir ~/MCALRUpdater/
 cp -f $WorkDir/UpdaterFiles/* ~/MCALRUpdater/
 /bin/bash ~/MCALRUpdater/Updater.sh
