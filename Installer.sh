02a18bba2aee1e36eae542459a3d48af38a2e3f5
M-Unity <mihailsheshukov10@gmail.com>
2025-09-02 23:54:24 +0500

    version 1.0

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WorkDir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-sudo pacman -S hyprland waybar fastfetch fish wofi uwsm kitty hyprpaper nano hyprlock pcmanfm-gtk3 nwg-look qalculate-gtk gvfs superfile ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono pipewire wireplumber xarchiver wl-clipboard cliphist grim hyprpolkitagent noto-fonts-extra plymouth mako noto-fonts-emoji imagemagick bashtop slurp
+sudo pacman -S hyprland waybar fastfetch fish rofi-wayland gnome-themes-extra uwsm kitty hyprpaper nano hyprlock nemo nwg-look qalculate-gtk gvfs superfile ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono pipewire wireplumber xarchiver wl-clipboard cliphist grim hyprpolkitagent noto-fonts-extra plymouth mako noto-fonts-emoji imagemagick bashtop slurp
 [ ! -d "~/MCALRUpdater/" ] && mkdir ~/MCALRUpdater/
 cp -f $WorkDir/UpdaterFiles/* ~/MCALRUpdater/
 /bin/bash ~/MCALRUpdater/Updater.sh
@@ -11,4 +11,4 @@ if ! command -v yay >/dev/null; then
 	cd $WorkDir
 	rm -rf $WorkDir/yay/
 fi
-yay -S adwaita-colors-icon-theme zen-browser-bin github-desktop-bin
+yay -S zen-browser-bin
--- UpdaterFiles/Updater.sh
+++ UpdaterFiles/Updater.sh
@@ -12,6 +12,9 @@ cp -rf $WorkDir/MCALR/ast/* $DataDir/MCALR/
 echo "Updating fastfetch config"
 rm -rf $HomeDir/.config/fastfetch
 cp -rf $WorkDir/MCALR/fastfetch $HomeDir/.config/
+echo "Updating nano config"
+rm -rf $HomeDir/.config/nano
+cp -rf $WorkDir/MCALR/nano $HomeDir/.config/
 echo "Updating hyprland configs"
 rm -rf $HomeDir/.config/hypr
 cp -rf $WorkDir/MCALR/hypr $HomeDir/.config/
@@ -24,9 +27,9 @@ cp -rf $WorkDir/MCALR/waybar $HomeDir/.config/
 echo "Updating fish config"
 rm -rf $HomeDir/.config/fish
 cp -rf $WorkDir/MCALR/fish $HomeDir/.config/
-echo "Updating wofi config"
-rm -rf $HomeDir/.config/wofi
-cp -rf $WorkDir/MCALR/wofi $HomeDir/.config/
+echo "Updating rofi config"
+rm -rf $HomeDir/.config/rofi
+cp -rf $WorkDir/MCALR/rofi $HomeDir/.config/
 echo "Updating Cursor Themes"
 rm -rf $DataDir/icons/cursors/
 cp -rf $WorkDir/MCALR/cursors/ $DataDir/icons/
@@ -36,9 +39,6 @@ for theme in "$DataDir/icons/cursors"/*; do
     ln -sf "$theme" "$DataDir/icons/$themename"
   fi
 done
-echo "Updating pcmanfm actions"
-rm -rf $DataDir/file-manager
-cp -rf $WorkDir/MCALR/file-manager $DataDir
 echo "Updating themes"
 rm -rf $HomeDir/.themes/*
 cp -rf $WorkDir/MCALR/themes/* $HomeDir/.themes/
@@ -49,11 +49,11 @@ cp -rf $WorkDir/MCALR/themes/* $HomeDir/.themes/
 [ -d "$PatchDir/kitty" ] && cp -rf $PatchDir/kitty $HomeDir/.config/
 [ -d "$PatchDir/waybar" ] && cp -rf $PatchDir/waybar $HomeDir/.config/
 [ -d "$PatchDir/fish" ] && cp -rf $PatchDir/fish $HomeDir/.config/
-[ -d "$PatchDir/wofi" ] && cp -rf $PatchDir/wofi $HomeDir/.config/
+[ -d "$PatchDir/wofi" ] && cp -rf $PatchDir/rofi $HomeDir/.config/
 hyprctl reload > /dev/null
 echo " "
-echo "﹍﹍﹍﹍﹍"
+echo "──────────"
 cat $WorkDir/MCALR/version
-echo "﹉﹉﹉﹉﹉"
+echo "──────────"
 echo " "
 rm -rf $WorkDir/MCALR
